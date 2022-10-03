8a2006a349fc9e61ca25cbe13103922dc6bcd3d8
Aleksandra Vorontsova <alexandrettio@gmail.com>
2022-10-04 01:49:59 +0300

    Small refactoring like logging format

--- tasks.py
+++ tasks.py
@@ -9,7 +9,8 @@ from model import YWResponse, CityWeatherData
 
 logging.basicConfig(
     filename="sprint1.log",
-    format="%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s"
+    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s",
+    level="INFO"
 )
 
 FileFormat = Literal["json", "csv", "xls"]
@@ -19,19 +20,24 @@ class DataFetchingTask:
     """Выкачивание данных от Яндекс.Погоды по названию города."""
 
     def __init__(self, city_name: str, yw_api: YandexWeatherAPI):
+        self.task_name = "DataFetchingTask"
         self.city_name = city_name
         self.yw_api = yw_api
 
     def run(self) -> YWResponse:
-        # ??? logging.info('Начинаю сбор сырых данных из URL')
+        logging.info(f"{self.task_name} started for city {self.city_name}.")
+
         try:
             raw_response = self.yw_api.get_forecasting(self.city_name)
-            return YWResponse.parse_obj(raw_response)
+            parsed = YWResponse.parse_obj(raw_response)
+            logging.info(f"{self.task_name} finished whithout errors for city {self.city_name}.")
+            return parsed
         except ValidationError as e:
-            logging.exception(f"Error during parsing response: {e}")
+            logging.exception(f"{self.task_name} finished with error for city "
+                              f"{self.city_name} during parsing response: {e}")
             raise e
         except Exception as e:  # Bad API: why raises common exception not customized?
-            logging.exception(f"processed city: {self.city_name}. Got: {e}")
+            logging.exception(f"{self.task_name} finished with error for city: {self.city_name}. Got: {e}")
             raise e
 
 
@@ -45,22 +51,28 @@ class DataCalculationTask:
     """
 
     def __init__(self, response: YWResponse):
+        self.task_name = "DataCalculationTask"
         self.response = response
 
     def run(self) -> List[CityWeatherData]:
+        logging.info(f"{self.task_name} started for city {self.response.geo_object.province.name}.")
+
         result = []
         base_conditions = ("rain", "snow", "hail", "thunderstorm")
         for date_info in self.response.forecasts:
             temps = [h.temp for h in date_info.hours if 9 <= h.hour <= 19]
+            without_conditions = [h.condition for h in date_info.hours
+                                  if 9 <= h.hour <= 19 and h.condition not in base_conditions]
             if temps:
                 data = CityWeatherData(
                     city=self.response.geo_object.province.name,
                     date=date_info.date,
                     average_temperature=sum(temps) / len(temps),
-                    without_conditions_hours=len([h.condition for h in date_info.hours
-                                                  if 9 <= h.hour <= 19 and h.condition not in base_conditions])
+                    without_conditions_hours=len(without_conditions)
                 )
                 result.append(data)
+        logging.info(f"{self.task_name} finished for city {self.response.geo_object.province.name}. "
+                     f"Got {len(result)} date weather records.")
         return result
 
 
@@ -68,10 +80,12 @@ class DataAggregationTask:
     """Группирует данные и создает таблицу с агрегированными данными."""
 
     def __init__(self, data: List[CityWeatherData], file_format: FileFormat):
-        self.data = data
-        self.format = file_format
+        self.task_name = "DataAggregationTask"
         self.avg_tmp_str = "Температура, среднее"
         self.no_conditions_str = "Без осадков, часов"
+
+        self.data = data
+        self.format = file_format
 
     @staticmethod
     def init_grouped_data_dict():
@@ -88,7 +102,7 @@ class DataAggregationTask:
                 grouped_data[(item.city, self.avg_tmp_str)] = self.init_grouped_data_dict()
                 grouped_data[(item.city, self.no_conditions_str)] = self.init_grouped_data_dict()
 
-            date = item.date.strftime("%d-%m")
+            date = item.date
             grouped_data[(item.city, self.avg_tmp_str)][date] = int(item.average_temperature)
             grouped_data[(item.city, self.avg_tmp_str)]["sum"] += item.average_temperature
             grouped_data[(item.city, self.avg_tmp_str)]["count"] += 1
@@ -118,15 +132,24 @@ class DataAggregationTask:
         return output_data, fieldnames
 
     def run(self):
+        logging.info(f"{self.task_name} started. File format is {self.format}.")
         if self.format == "csv":
             with open("report.csv", "w") as f:
                 data, fields = self.group_for_output()
                 writer = csv.DictWriter(f, fields)
                 writer.writeheader()
                 writer.writerows(data)
+        logging.info(f"{self.task_name} finished.")
 
 
 class DataAnalyzingTask:
+    """
+    Составляет рейтинг привлекательности городов
+
+    - Наиболее благоприятным городом считать тот, в котором средняя температура за всё время была самой высокой,
+    а количество времени без осадков — максимальным. Если таких городов более одного, то выводить все.
+    """
+
     def __init__(self):
         pass
 
